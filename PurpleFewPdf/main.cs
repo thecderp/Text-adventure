978ab0d9f8cb84edb876725f04aa7a7123db85b7
thecderp <70461970+thecderp@users.noreply.github.com>
2020-09-13 12:26:08 +1000

    (Unzipped) Base Code
    Second time ain't a charm, but this better work 0_o

---
+++ PurpleFewPdf/main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+//Just Setting stuff up here
+//This is the base code of the game, this may be edited or have a pull request or a copy, idk for now
+
+  namespace BaseCode
+  {
+    public class Program
+    {
+      public static void Main(string[] args)
+      {
+        Console.Write("Hello there");
+      }
+    }
+  }
