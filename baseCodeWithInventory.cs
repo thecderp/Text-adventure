b95d37899c9641e1e9a133d3bbdf006051ffbb62
thecderp <70461970+thecderp@users.noreply.github.com>
2020-09-13 15:19:35 +1000

    Update baseCodeWithInventory.cs
    This has more stuff, but is super unstable. Because of this, it will be on a different branch.

--- baseCodeWithInventory.cs
+++ baseCodeWithInventory.cs
@@ -11,10 +11,24 @@ using System.Text.RegularExpressions;
     {
       public static void Main(string[] args)
       {
-        //I want to try to add a list for inventory
+        //I want to try to add a list for inventory vvv
         List<string> inventory = new List<string>(5);
-        //Heres the "Hello there" message
-        Console.Write("█ █ █▀▀ █▄█   ▀█▀ █ █ █▀▀ █▀█ █▀▀\n█▀█ ██▄  █     █  █▀█ ██▄ █▀▄ ██▄ ");
+        //Heres the "Hello there" message vvv
+        Console.WriteLine("\nHey there\nWelcome to Text Adventure!");
+        Console.Write("Type 'S' to start");
+        Console.ReadLine();
+        string pressPlay = Console.ReadLine();
+        //This code here will be temporary vvv
+        Console.WriteLine("Here's the stuff in your inventory:"); //Learn how to have a limit on the inventory pls(5 items)
+        //This code will just put stuff in the iventory vvv
+        inventory.add("Book");
+        inventory.add("Hat");
+        //Let's read that stuff
+        for (int i = 0; i < 2; i++)
+        {
+          Console.WriteLine("inventory");
+        }
+        //This bit of code helps the terminal stay open when the code runs vvv
         Console.ReadKey();
       }
     }
