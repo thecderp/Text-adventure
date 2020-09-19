7d52f1a06fdefb95a4d668a3d4367f1cbf1d2cf8
thecderp <70461970+thecderp@users.noreply.github.com>
2020-09-19 14:01:08 +1000

    Update and rename main.cs to TextAdventureCode.cs
    Alot of stufff added, removed and changed.

--- TextAdventureCode.cs
+++ TextAdventureCode.cs
@@ -3,27 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
+//The class for the player
 public class Player {
   public string playerName;
   public int playerHealth;
   public int playerSpeed;
   public int playerStrength;
   //Yayy, my first time using classes.
-  static void SpawnPlayer() {
-    //code to execute
-  }
+  public static void SpawnPlayer() {
+    Player mainPlayer = new Player();
+    mainPlayer.playerName = "Hag";
+    mainPlayer.playerHealth = 3;
+    mainPlayer.playerSpeed = 2;
+    mainPlayer.playerStrength = 2;
 }
 
+//The class for items
 public class Items {
   public string itemName;
   public string itemDescription;
-  
+  //attempting to make an Item
+  public static void MakeItem() {
+
+  }
+}
+
 }
 //This class here is the main one, and will stay. vvv
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine("Hello World");
-    Console.Write("What is your player's name?");
-    Console.ReadLine();
+    Console.WriteLine("Welcome to 'Text Adventure'");
+    Player.SpawnPlayer();
+
   }
 }
