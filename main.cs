0aac3920d051f519ce9047742cfc1a5c3396c9b7
thecderp <70461970+thecderp@users.noreply.github.com>
2020-09-19 16:04:43 +1000

    Update main.cs
    just something that kindof works, this'll be fixed

--- main.cs
+++ main.cs
@@ -3,23 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
+//The class for the player
 public class Player {
-  string playerName = "Hag";
-  int playerHealth = 3;
-  int playerSpeed = 0;
-  int playerStrength = 0;
+  public string playerName;
+  public int playerHealth;
+  public int playerSpeed;
+  public int playerStrength;
   //Yayy, my first time using classes.
+  public static void SpawnPlayer() {
+    Player mainPlayer = new Player();
+    mainPlayer.playerName = "Hag";
+    mainPlayer.playerHealth = 3;
+    mainPlayer.playerSpeed = 2;
+    mainPlayer.playerStrength = 2;
 }
 
+//The class for items
+public class Item {
+  public string itemName { get; set; }
+  public string itemDescription { get; set; }
+}
+
+}
 //This class here is the main one, and will stay. vvv
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-    //This is just a test, this will be removed vvv
-    Console.WriteLine("playerName = ");
-    Console.WriteLine("playerHealth = ");
-    Console.WriteLine("playerSpeed = ");
-    Console.WriteLine("playerStrength = ");
-    Console.ReadLine();
+    Console.WriteLine("Welcome to 'Text Adventure'");
+    Player.SpawnPlayer();
+    Item i = new Item();
   }
 }
