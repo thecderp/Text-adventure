6a0e6c4df65947cbf7f1fee90e73502d5325fbfa
thecderp <70461970+thecderp@users.noreply.github.com>
2020-09-26 15:33:57 +1000

    Added the folder for the project (the new one)
    Yep, this is pretty much it. Thsi will have more files in it, but yeah. This branch will merge into master pretty soon.

---
+++ TextAdventure/Items.cs
@@ -0,0 +1,8 @@
+namespace TextAdventureCode
+
+	//The class for items
+	public class Item {
+		public string itemName { get; set; }
+		public string itemDescription { get; set; }
+		//attempting to make an item
+	}
