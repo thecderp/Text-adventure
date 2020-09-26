325364a3655457e6f2e2eb58c84cc2fd5fe3401c
thecderp <70461970+thecderp@users.noreply.github.com>
2020-09-26 15:38:39 +1000

    Update and rename Items.cs to Item.cs
    Just a little fix to the '{}'

--- TextAdventure/Item.cs
+++ TextAdventure/Item.cs
@@ -1,8 +1,9 @@
 namespace TextAdventureCode
-
+{
 	//The class for items
 	public class Item {
 		public string itemName { get; set; }
 		public string itemDescription { get; set; }
 		//attempting to make an item
 	}
+}
