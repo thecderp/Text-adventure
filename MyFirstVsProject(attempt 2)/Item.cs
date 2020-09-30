132e404a0ca1cf4ed1ff6bcef414533ffc66a6c8
thecderp <70461970+thecderp@users.noreply.github.com>
2020-09-30 17:17:39 +1000

    The full thang
    xD lol. I use VS now, and for some reason, all these other files are here, but its fine.

---
+++ MyFirstVsProject(attempt 2)/Item.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextAdventureCode
+{
+
+	//The class for items
+	public class Item
+	{
+		public string itemName { get; set; }
+		public string itemDescription { get; set; }
+		//attempting to make an item
+	}
+}
