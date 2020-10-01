4cacc9aadb1a68f8e97e93b2a71e0a850c63a2dd
thecderp <70461970+thecderp@users.noreply.github.com>
2020-10-01 11:50:09 +1000

    Delete Item.cs
    Gotta delete this

--- TextAdventure/Item.cs
+++
@@ -1,9 +0,0 @@
-namespace TextAdventureCode {
-	
-	//The class for items
-	public class Item {
-		public string itemName { get; set; }
-		public string itemDescription { get; set; }
-		//attempting to make an item
-	}
-}
