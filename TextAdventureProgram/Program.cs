e3f2dc0baf2361149103c65cbb7be879ad483c5f
thecderp <70461970+thecderp@users.noreply.github.com>
2020-10-01 12:03:50 +1000

    The Project upload(attempt 2 lol)
    Whoops, I kinda had to nuke some bits of the repo because I messed up aa little thing on attempt 1

---
+++ TextAdventureProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextAdventureCode
+{
+
+    public class Program
+    {
+        //The main thingy
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to 'Text Adventure'");
+            Console.ReadKey();
+        }
+    }
+}
